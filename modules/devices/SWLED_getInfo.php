5122a320a74c149c26423b298008a4ff3bea3fad
Исупов Андрей Николаевич <isupov.a@niisvt.ru>
2020-06-30 07:40:09 +0300

    fix get info

--- modules/devices/SWLED_getInfo.php
+++ modules/devices/SWLED_getInfo.php
@@ -2,7 +2,7 @@
 
 //get information
 $url = 'http://'. $this->getProperty("ip")."/json";
-$content = file_get_contents($url);
+$content = getUrl($url);
 $json = json_decode($content, true);
 $this->setProperty("effects", json_encode($json["effects"]));
 $this->setProperty("palettes", json_encode($json["palettes"]));
